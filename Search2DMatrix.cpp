474af099878e40cea27d8f9d68fc023782a9a20c
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-06-08 21:11:19 +0530

    Create Search2DMatrix.cpp

---
+++ Search2DMatrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      int n=matrix.size();
+      int m=matrix[0].size();
+
+      int count=0;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==target){
+                count=1;
+                break;
+            }
+        }
+      }
+        if(count)return true;
+        return false;
+      }
+}; 
+
+//tc: o(n^2)
+//sc: o(1)
