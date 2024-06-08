9ab5b858c3079745987df2863f020be5751f91a9
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-06-08 21:44:33 +0530

    Create MajorityElement_O(1)Space.cpp

---
+++ MajorityElement_O(1)Space.cpp
@@ -0,0 +1,31 @@
+///linear time and in O(1) space
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int count=0;
+       int element=0;
+       
+       for(int num:nums){
+        if(count==0){
+            element=num;
+        }
+        
+        if(num==element){
+            count+=1;
+
+        }
+        else{
+            count-=1;
+        }
+       }
+        return element;
+       }
+
+
+
+
+    
+};
+
+//tc: o(n)
+//sc: o(1)
