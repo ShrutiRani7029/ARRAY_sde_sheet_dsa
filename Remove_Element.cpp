b64e85415931f96ee070f62b2b88019f75f045b6
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2024-07-16 19:39:34 +0530

    Create Remove_Element.cpp
    Time Complexity: O(n^2)
    Space Complexity: O(1)
    
    -Time Complexity
    Loop Execution: The while loop runs v.size() times, where v.size() is dynamic because the size of the vector decreases as elements are removed.
    Erase Operation: v.erase(v.begin() + i) is an O(n) operation in the worst case because it needs to shift all elements after the removed element.
    
    -The function uses a constant amount of extra space:
    
    Integer i is used for indexing.
    No additional data structures are used that grow with the size of the input vector v.
    Thus, the space complexity is O(1).

---
+++ Remove_Element.cpp
@@ -0,0 +1,10 @@
+ int removeElement(vector<int>& v, int val) {
+       int i=0;
+        while(i<v.size()){
+            if(v[i]==val){
+                v.erase(v.begin()+i);
+            }
+            else i++;
+        }
+        return v.size();
+    }
